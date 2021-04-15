da847c8c05fcd63b7b9755bcc16297d315b934de
Ian Y.E. Pan <ianpan870102@gmail.com>
2021-04-15 15:28:52 +0800

    Update tree-sitter support (make if/else/for/while/return magenta) to match VSCode

--- vscode-dark-plus-theme.el
+++ vscode-dark-plus-theme.el
@@ -538,6 +538,7 @@
    `(tree-sitter-hl-face:type.builtin         ((t (:inherit font-lock-keyword-face))))
    `(tree-sitter-hl-face:number               ((t (:inherit highlight-numbers-number))))
    `(tree-sitter-hl-face:variable.special     ((t (:inherit font-lock-keyword-face))))
+   `(tree-sitter-hl-face:keyword              ((t (:inherit ,ms-magenta))))
 
    `(lsp-ui-doc-background                    ((t (:background ,bg2))))
 
